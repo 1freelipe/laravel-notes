bb97434e9fcfe4efd3eb4660ddd9a0563ebd9947
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-04-09 15:34:37 -0300

    limpando o projeto para de fato começar a desenvolver e mudando algumas configurações de database

--- resources/views/page3.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main_layout')
-
-@section('content')
-    <h1>Welcome View and Controller</h1>
-    <hr>
-    <h3>Page 3</h3>
-    <h3>This value is: {{ $value }} </h3>
-@endsection
