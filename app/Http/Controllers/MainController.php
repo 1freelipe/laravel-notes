c76bf30a7834aade3543b5b658cc8d08904331e0
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-04-15 11:52:05 -0300

    criando middlewares para usuarios autenticados e não autenticados

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index() {
+        echo 'Im inside the app!';
+    }
+
+    public function newNote() {
+        echo 'I am a create a new note';
+    }
 
 }
