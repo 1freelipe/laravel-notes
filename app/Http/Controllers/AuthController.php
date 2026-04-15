a8c996809a729cad13aafb6e2237555f05b5d5f6
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-04-15 09:06:56 -0300

    criando validação de usuário e senha com condições e exibindo os erros diretamente para o usuário utilizando blade template

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,8 +13,16 @@ class AuthController extends Controller
     public function loginSubmit(Request $request) {
         // Form validation
         $request->validate([
-            'text_username' => 'required',
-            'text_password' => 'required'
+            // rules
+            'text_username' => 'required|email',
+            'text_password' => 'required|min:6|max:16'
+        ], [
+            // error messages
+            'text_username.required' => 'O username é obrigatório.',
+            'text_username.email' => 'Username deve ser um e-mail válido.',
+            'text_password.required' => 'A senha é obrigatória.',
+            'text_password.min' => 'A senha deve conter no mínimo :min caracteres.',
+            'text_password.max' => 'A senha deve conter no máximo :max caracteres.'
         ]);
 
         // Get user input
